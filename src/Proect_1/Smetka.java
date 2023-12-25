0b5f55150bdc60529a11d0b12b97aed09f098af3
KostoPodjanin <kostopodjanin@gmail.com>
2023-12-25 18:42:15 +0100

    proekt

---
+++ src/Proect_1/Smetka.java
@@ -0,0 +1,14 @@
+package Proect_1;
+
+	public class Smetka {
+
+		public static void main(String[] args) {
+			int jab=32;
+			int komp=20;
+			int pras=50;
+			double rezultat;
+			rezultat=(jab*2)+(komp*5)+(pras*2.5);
+			System.out.println("Vkupno vo denari:");
+			System.out.println(rezultat);
+		}
+	}
